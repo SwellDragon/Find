a5bb649235b2eafd8fdebc395bb8b988649e39f4
SwellDragon <624523249@qq.com>
2018-06-05 20:55:44 +0800

    change

--- app/src/main/java/com/swelldragon/find/MainActivity.java
+++ app/src/main/java/com/swelldragon/find/MainActivity.java
@@ -3,18 +3,31 @@ package com.swelldragon.find;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.internal.BottomNavigationItemView;
 import android.support.design.internal.BottomNavigationMenuView;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
 
+
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 class BottomNavigationViewHelper {
     @SuppressLint("RestrictedApi")
@@ -40,12 +53,35 @@ class BottomNavigationViewHelper {
         }
     }
 }
+class ViewPagerAdapter extends FragmentPagerAdapter {
 
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+
+    public ViewPagerAdapter(FragmentManager manager) {
+        super(manager);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment) {
+        mFragmentList.add(fragment);
+    }
+
+}
 
 public class MainActivity extends AppCompatActivity {
-
+    private ViewPager viewPager;
     private TextView mTextMessage;
-
+    private MenuItem menuItem;
+    private BottomNavigationView navigation;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -53,15 +89,19 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
+                    viewPager.setCurrentItem(0);
                     mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
+                    viewPager.setCurrentItem(1);
                     mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
+                    viewPager.setCurrentItem(2);
                     mTextMessage.setText(R.string.title_notifications);
                     return true;
                 case R.id.aaa:
+                    viewPager.setCurrentItem(3);
                     mTextMessage.setText("我");
                     return true;
             }
@@ -81,10 +121,45 @@ public class MainActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.hide();
         }
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        BottomNavigationViewHelper.disableShiftMode(navigation);
+        navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
         mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationViewHelper.disableShiftMode(navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (menuItem != null) {
+                    menuItem.setChecked(false);
+                } else {
+                    navigation.getMenu().getItem(0).setChecked(false);
+                }
+                menuItem = navigation.getMenu().getItem(position);
+                menuItem.setChecked(true);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+        setupViewPager(viewPager);
+
     }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(BaseFragment.newInstance("新闻"));
+        adapter.addFragment(BaseFragment.newInstance("图书"));
+        adapter.addFragment(BaseFragment.newInstance("发现"));
+        adapter.addFragment(BaseFragment.newInstance("更多"));
+        viewPager.setAdapter(adapter);
+    }
+
 
 }
